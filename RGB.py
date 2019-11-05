49fdf394f9333209cd650fe4c624b26123f05a04
Ben Lapuhapo <ben.lapuhapo@mths.ca>
2019-11-05 23:30:11 +0000

    Changed published date

--- RGB.py
+++ RGB.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Created by: Ben Lapuhapo
-# Created on: OCT 2019
+# Created on: Nov 2019
 # This program is a guessing game
 
 
@@ -13,12 +13,10 @@ def color():
     blue = 0
 
     # process & output
-    for red in range(0, 256):
-        for green in range(0, 256):
-            for blue in range(0, 256):
-                if (red, green, blue == 255):
-                    print("RGB Value is: ({0}), ({1}), ({2})"
-                          .format(red, green, blue))
+    for red in range(1, 256):
+        for green in range(1, 256):
+            for blue in range(1, 256):
+                print("RGB Value is: ({0}, {1}, {2})".format(red, green, blue))
 
 
 if __name__ == "__main__":
